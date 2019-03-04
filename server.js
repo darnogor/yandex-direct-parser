860d1f7161d5ca3504f90781614d59b32533d520
Alexander Petrenko <j.alex.ptr@gmail.com>
2019-03-04 18:57:04 +0300

    Add hello world

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var app = express();
 
+
 app.get('/', function (req, res) {
     res.render('index.html');
+});
+
+
+var port = process.env.port || 8080;
+
+app.use(express.static(__dirname));
+app.use(express.static(__dirname + 'view'));
+
+app.listen(port, function () {
+    console.log('app running');
 });
