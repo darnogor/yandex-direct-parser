5dfa59555263044b5c739e3c2fcd24c696882869
Alexander Petrenko <j.alex.ptr@gmail.com>
2019-03-05 17:20:18 +0300

    Fix

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ let express = require('express');
 let app = express();
 
 
-app.get('*', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/view/index.html');
 });
 
-app.post('/ads/get', function (req, res) {
-    let words = req.params.query;
-});
+// app.post('/ads/get', function (req, res) {
+//     let words = req.params.query;
+// });
 
 app.use(express.static('view'));
 
-const port = process.env.PORT || 5000;
+const port = process.env.port || 5000;
 app.listen(port, function () {
     console.log('app running');
 });
