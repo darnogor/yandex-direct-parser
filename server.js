6abc1263e3b3210970a1fcd86123b9bd3340eb07
Alexander Petrenko <j.alex.ptr@gmail.com>
2019-03-07 16:18:34 +0300

    Add yandex

--- server.js
+++ server.js
@@ -1,23 +1,68 @@
 const express    = require('express');
 const bodyParser = require('body-parser');
 const path       = require('path');
-const yandex     = require('yandex-direct-api');
+const https      = require('https');
+const yandexAPI  = require('yandex-direct-api');
 const port       = process.env.PORT || 5000;
+
+
+let yandex = {
+    token: null,
+    api: null,
+
+    init: () => {
+        if (!this.isValid()) {
+            https.request('https://oauth.yandex.ru/authorize', {
+                response_type: token,
+                client_id: '6aab4935cb0145ebb2f3df72e9f2f079'
+            }, (res) => {
+                console.log(res);
+            });
+        }
+    },
+    initWithToken: (token) => {
+        if (token) {
+            this.token = token;
+            this.api = yandexAPI({
+                token: token,
+                locale: 'ru'
+            });
+        }
+    },
+    isValid: () => {
+        let isValid = this.token !== null && this.token !== '' && this.api !== null;
+        console.log('sdfsdfsdfsdf' + isValid);
+        return isValid;
+    }
+};
+
 
 let app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'view')));
 app.set('views', path.join(__dirname, 'view'));
 
+function ignoreFavicon(req, res, next) {
+    if (req.originalUrl === '/favicon.ico') {
+        res.status(204).json({nope: true});
+    } else {
+        next();
+    }
+}
+app.use(ignoreFavicon);
+
 app.get('/', (req, res) => {
-    res.sendFile('index.html');
+    if (yandex.isValid()) {
+        res.sendFile('index.html');
+    } else {
+        yandex.init();
+    }
 });
 
 app.post('/ads/get', (req, res) => {
     let words = req.body.query;
+    // yandex.api.call();
     res.json([{
         name: words,
         contact: ''
